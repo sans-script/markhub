19b5c659279a673d4a9eaa2454ae03a81ec63d08
sans-script <alexhere.user@gmail.com>
2025-01-25 22:04:00 -0300

    feat: integrate Toast notifications and ErrorBoundary for improved error handling and user feedback

--- app/page.js
+++ app/page.js
@@ -8,14 +8,20 @@ import InputField from "./components/InputField";
 import "highlight.js/styles/github-dark.css";
 import "github-markdown-css";
 import Sidebar from "./components/Sidebar";
+import Toast from "./components/Toast";
+import ErrorBoundary from "./utils/ErrorBoundary";
+import editorGuideContent from "./examples/editorGuideContent";
 
 export default function Home() {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState(editorGuideContent);
   const [editorWidth, setEditorWidth] = useState(50);
   const [bottomDivHeight, setBottomDivHeight] = useState(20);
   const [sidebarWidth, setSidebarWidth] = useState(3);
   const [prompt, setPrompt] = useState("");
   const [isTransitioning, setIsTransitioning] = useState(false);
+  const [toastMessage, setToastMessage] = useState("");
+  const [toastType, setToastType] = useState("");
+  const [errorKey, setErrorKey] = useState(0);
 
   const toggleEditor = () => {
     setIsTransitioning(true);
@@ -71,12 +77,15 @@ export default function Home() {
   const saveAsMarkdown = async () => {
     try {
       if (!input || input.trim() === "") {
-        alert("Cannot save an empty file. Please add content before saving.");
+        setToastMessage(
+          "Cannot save an empty file. Please add content before saving."
+        );
+        setToastType("error");
         return;
       }
 
       const fileHandle = await window.showSaveFilePicker({
-        suggestedName: "file.md",
+        suggestedName: "README.md",
         types: [
           {
             description: "Markdown Files",
@@ -89,13 +98,20 @@ export default function Home() {
       await writable.write(input);
       await writable.close();
 
-      alert("File saved successfully!");
+      setToastMessage("File saved successfully!");
+      setToastType("success");
     } catch (error) {
       if (error.name !== "AbortError") {
         console.error("Error saving the file:", error);
-        alert("An error occurred while saving the file.");
+        setToastMessage("An error occurred while saving the file.");
+        setToastType("error");
       }
     }
+  };
+
+  const handleCloseToast = () => {
+    setToastMessage("");
+    setToastType("");
   };
 
   const openMarkdownFile = async () => {
@@ -114,27 +130,40 @@ export default function Home() {
       const content = await file.text();
 
       if (content.trim() === "") {
-        alert("The file is empty.");
+        setToastMessage("The file is empty.");
+        setToastType("error");
         return;
       }
 
       setInput(content);
-      // alert(`File "${file.name}" loaded successfully!`);
+      setToastMessage(`File "${file.name}" loaded successfully!`);
+      setToastType("success");
     } catch (error) {
       if (error.name === "AbortError") {
         console.log("File selection was canceled.");
       } else {
         console.error("Error opening the file:", error);
-        alert("An error occurred while opening the file.");
+        setToastMessage("An error occurred while opening the file.");
+        setToastType("error");
       }
     }
+  };
+
+  const handleInputChange = (newInput) => {
+    setInput(newInput);
+    setErrorKey((prevKey) => prevKey + 1); // Atualiza o erroKey para forçar re-renderização
   };
 
   useEffect(() => {
     const observer = new MutationObserver(() => {
       document.querySelectorAll("pre").forEach((element) => {
-        if (element.querySelector("code.language-mermaid")) {
-          element.style.backgroundColor = "white";
+        const codeElement = element.querySelector("code");
+        if (codeElement) {
+          if (codeElement.classList.contains("language-mermaid")) {
+            element.style.backgroundColor = "white";
+          } else {
+            element.style.backgroundColor = "";
+          }
         }
       });
     });
@@ -197,7 +226,7 @@ export default function Home() {
             {/* Monaco Editor Box */}
             <Editor
               input={input}
-              setInput={setInput}
+              setInput={handleInputChange}
               bottomDivHeight={bottomDivHeight}
               editorWidth={editorWidth}
               isTransitioning={isTransitioning}
@@ -206,12 +235,14 @@ export default function Home() {
             />
 
             {/* Preview Container */}
-            <Preview
-              input={input}
-              editorWidth={editorWidth}
-              bottomDivHeight={bottomDivHeight}
-              isTransitioning={isTransitioning}
-            />
+            <ErrorBoundary key={errorKey}>
+              <Preview
+                input={input}
+                editorWidth={editorWidth}
+                bottomDivHeight={bottomDivHeight}
+                isTransitioning={isTransitioning}
+              />
+            </ErrorBoundary>
           </div>
 
           {/* Input Field */}
@@ -222,6 +253,14 @@ export default function Home() {
             setBottomDivHeight={setBottomDivHeight}
             isTransitioning={isTransitioning}
           />
+
+          {toastMessage && (
+            <Toast
+              message={toastMessage}
+              onClose={handleCloseToast}
+              type={toastType}
+            />
+          )}
         </div>
       </div>
     </div>
