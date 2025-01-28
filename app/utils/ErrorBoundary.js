cc13bb1f899eeb18948826988d423ef86f328304
sans-script <alexhere.user@gmail.com>
2025-01-28 16:43:55 -0300

    fix: remove commented code in ErrorBoundary component for clarity

--- app/utils/ErrorBoundary.js
+++ app/utils/ErrorBoundary.js
@@ -8,7 +8,7 @@ class ErrorBoundary extends React.Component {
   }
 
   static getDerivedStateFromError(error) {
-    return { hasError: true, showToast: true }; // Ativa o toast ao capturar o erro
+    return { hasError: true, showToast: true };
   }
 
   componentDidCatch(error, errorInfo) {
@@ -16,14 +16,14 @@ class ErrorBoundary extends React.Component {
   }
 
   handleCloseToast = () => {
-    this.setState({ showToast: false }); // Fecha o toast
+    this.setState({ showToast: false });
   };
 
   render() {
     const { hasError, showToast } = this.state;
     const { children, errorKey } = this.props;
 
-    // Se houve um erro, renderiza o erro ou o toast
+    
     return (
       <>
         {showToast && (
